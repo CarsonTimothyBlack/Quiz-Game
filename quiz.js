6f16a2e1aa4a58455c9347204fa417b9bfc925ec
Carson Black <37965925+CarsonTimothyBlack@users.noreply.github.com>
2018-10-26 15:20:17 -0400

    Added enhancements from treehouse
    Storing this updated code from treehouse quiz solution to reference.

--- quiz.js
+++ quiz.js
@@ -2,25 +2,45 @@ var quiz = [
   ['What is your name?', 'Carson'],
   ['What is your quest?', 'Holy Grail'],
   ['What is your favorite color?', 'Green']
-]
+];
 
-var correct = 0;
-var wrong = 0;
+var correct = [];
+var wrong = [];
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var html;
 
-for (var i = 0; i < 3; i += 1) {
-  var ans = prompt(quiz[i][0]);
-  if (ans === quiz[i][1]){
-    alert('Correct!');
-    correct += 1;
-    print('<p>You answered the question "' + quiz[i][0] + '" correctly.</p>');
+function print(message) {
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
+}
+
+function buildList(arr) {
+  var listHTML = '<ol>';
+    for (var i = 0; i < arr.length; i += 1) {
+      listHTML += '<li>' + arr[i] + '</li>';
+  }
+  listHTML += '</ol>'
+  return listHTML;
+}
+
+for (var i = 0; i < quiz.length; i += 1) {
+  question = quiz[i][0];
+  answer = quiz[i][1];
+  response = prompt(question);
+  if (response === answer) {
+    correctAnswers += 1;
+    correct.push(question);
   } else {
-    alert('Wrong!');
-    wrong += 1;
-    print('<p>You answered the question "' + quiz[i][0] + '" incorrectly.</p>');
+    wrong.push(question);
   }
 }
 
-print('You answered ' + correct + ' out of 3 questions correctly! Good job!');
-function print(message) {
-  document.write(message);
-}
+html = "You got " + correctAnswers + " question(s) right."
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2>You got these questions wrong:</h2>'
+html += buildList(wrong);
+print(html);
